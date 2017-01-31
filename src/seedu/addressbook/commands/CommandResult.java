e2de919cd821a7f625d4d95b5c9903bb23db5e09
Mustaqiim Muhar <mustaqiim.muhar@u.nus.edu>
2017-01-31 22:01:38 +0800

    Apply Encapsulation

--- src/seedu/addressbook/commands/CommandResult.java
+++ src/seedu/addressbook/commands/CommandResult.java
@@ -11,10 +11,18 @@ import java.util.Optional;
 public class CommandResult {
 
     /** The feedback message to be shown to the user. Contains a description of the execution result */
-    public final String feedbackToUser;
+    private String feedbackToUser;
 
     /** The list of persons that was produced by the command */
     private final List<? extends ReadOnlyPerson> relevantPersons;
+    
+    public String getfeedbackToUser(){
+    	return feedbackToUser;
+    }
+    
+    public void setfeedbackToUser(String feedbackToUser){
+    	this.feedbackToUser = feedbackToUser;
+    }
 
     public CommandResult(String feedbackToUser) {
         this.feedbackToUser = feedbackToUser;
